4778c77353f886e3e187a5ef575f20346d61229f
Gurneet Bhatia <sbgurneet@gmail.com>
2020-05-27 13:25:35 +0400

    instantiates a matrix

--- pypmatrix/__init__.py
+++ pypmatrix/__init__.py
@@ -1,3 +1,6 @@
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+"""
+
+"""
+class Matrix(object):
+    def __init__(self, rows, cols, fill=0):
+        self.elements = [[fill for col in range(cols)] for row in range(rows)]
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='pypmatrix',
       version='0.1',
       description='Package to manage and manipulate matrices',
       url='https://github.com/gurneetbhatia/pypmatrix',
-      author='Flying Circus',
+      author='Gurneet Bhatia',
       author_email='sbgurneet@gmail.com',
       license='MIT',
       packages=['pypmatrix'],
