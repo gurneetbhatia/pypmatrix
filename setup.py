c14e03c532cb6c42cc7d28fcfa92d14d82de7051
Gurneet Bhatia <sbgurneet@gmail.com>
2020-05-27 12:31:41 +0400

    testing pip tutorial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='Package to manage and manipulate matrices',
+      url='https://github.com/gurneetbhatia/pypmatrix',
+      author='Flying Circus',
+      author_email='sbgurneet@gmail.com',
+      license='MIT',
+      packages=['pypmatrix'],
+      zip_safe=False)
