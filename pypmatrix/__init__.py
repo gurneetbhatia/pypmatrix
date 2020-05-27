62e5211b74d1cc3dcd376724fbe9cc853ab01c5d
Gurneet Bhatia <sbgurneet@gmail.com>
2020-05-27 14:06:54 +0400

    can instantiate from strings and lists

--- pypmatrix/__init__.py
+++ pypmatrix/__init__.py
@@ -1,6 +1,45 @@
-"""
+from fractions import Fraction as frac
 
-"""
+
+class MatrixError(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class InequalRowLengthsError(MatrixError):
+    """Raised when the row lengths are not all the same"""
+    def __init__(self, message="Row lengths in the matrix are not consistent!"):
+        super().__init__(message)
+
+class InvalidMatrixElementError(MatrixError):
+    """raised when any of the elements in the matrix cannot be processed as
+    a fraction object"""
+    def __init__(self, message="Matrix contains invalid non-fractional value!"):
+        super().__init__(message)
+
 class Matrix(object):
+    @staticmethod
+    def from_list(el_list):
+        m = Matrix(len(el_list), len(el_list[0]))
+        row_len = len(el_list[0])
+        invalid_el = False
+        for (index, row) in enumerate(el_list):
+            if len(row) != row_len:
+                raise InequalRowLengthsError
+            try:
+                m.elements[index] = [frac(el) for el in row]
+            except ValueError:
+                invalid_el = True
+                break
+        if invalid_el:
+            raise InvalidMatrixElementError
+        return m
+
+    @staticmethod
+    def from_string(string, rowsep=None, colsep=None):
+        el_list = [row.split(colsep) for row in string.strip().split(
+        rowsep if rowsep != None else '\n'
+        )]
+        return Matrix.from_list(el_list)
+
     def __init__(self, rows, cols, fill=0):
         self.elements = [[fill for col in range(cols)] for row in range(rows)]
