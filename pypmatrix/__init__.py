dccdc756ed9fcccc3c05b4284a29ae07875dcda0
Gurneet Bhatia <sbgurneet@gmail.com>
2020-05-27 17:21:14 +0400

    determinant of matrix works

--- pypmatrix/__init__.py
+++ pypmatrix/__init__.py
@@ -16,6 +16,12 @@ class InvalidMatrixElementError(MatrixError):
     def __init__(self, message="Matrix contains invalid non-fractional value!"):
         super().__init__(message)
 
+class MatrixNotSquareError(MatrixError):
+    """raised when you try carrying out an operation that only works on
+    square matrices on a non-square matrix"""
+    def __init__(self, message="Matrix not square!"):
+        super().__init__(message)
+
 class Matrix(object):
     @staticmethod
     def from_list(el_list):
@@ -44,6 +50,44 @@ class Matrix(object):
     def __init__(self, rows, cols, fill=0):
         self.elements = [[fill for col in range(cols)] for row in range(rows)]
 
+    def cofactor(self, row, col):
+        # returns the specified cofactor
+        i, j = row+1, col+1
+        return pow(i, j) * self.minor(row, col)
+
+    def det(self):
+        # returns the determinant of the matrix if it is square
+        if self.is_square():
+            if len(self.elements) == 1:
+                return self.elements[0][0]
+            # now find and return the determinant of the matrix
+            if self.size() == (2, 2):
+                return ((self.elements[0][0]*self.elements[1][1]) -
+                (self.elements[0][1]*self.elements[1][0]))
+            else:
+                determinant = 0
+                for (index, value) in enumerate(self.elements[0]):
+                    # iterate through the top row of the matrix
+                    # take out the top row from the matrix copy
+                    submatrix = self.elements[1:]
+                    # take out the current column (index) from the matrix
+                    submatrix = list(map(
+                    lambda row: row[:index]+row[index+1:], submatrix))
+                    s = "+" if pow(-1, index) == 1 else "-"
+                    determinant += (pow(-1, index) * value *
+                                    Matrix.from_list(submatrix).det())
+                return determinant
+        else:
+            raise MatrixNotSquareError
+
+    def is_square(self):
+        # returns true or false to indicate whether a matrix is square or not
+        return len(self.elements) == len(self.elements[0])
+
+    def size(self):
+        # returns a tuple with number of rows followed by number of columns
+        return (len(self.elements), len(self.elements[0]))
+
     def trans(self):
         # returns the transpose of the matrix
         m = Matrix(len(self.elements[0]), len(self.elements))
@@ -53,8 +97,11 @@ class Matrix(object):
         return m
 
 el_list = [
-[1,2],
-[4,5],
-[7,8]
+[1,34,57,6],
+[762,6,10,100],
+[828,2,18,990],
+[9,100,7628,9292]
 ]
-print(Matrix.from_list(el_list).trans().elements)
+matrix = Matrix.from_list(el_list)
+# print(matrix.trans().elements)
+print(matrix.det())
